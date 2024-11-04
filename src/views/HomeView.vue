afc03fd7d8024b664aa32c52812851c6d6473cd2
buberrie <83277559+buberrie@users.noreply.github.com>
2024-11-04 18:33:32 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,10 @@
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center text-center">
+  <div class="flex flex-col justify-center items-center text-center mt-10">
     <h1>Welcome to the Road Trip Planner!</h1>
     <p>Plan your next adventure by adding destinations and viewing the best routes.</p>
-    <router-link to="/destinations">Go to Destinations</router-link>
+    <router-link to="/destinations" class="text-blue-500">Go to Destinations</router-link>
   </div>
 </template>
 
